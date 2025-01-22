c9f5a824f2fd2e9a4d055eb5a33791b71ebe857b
Guntur Poetra <poetra.guntur@gmail.com>
2025-01-22 21:08:51 +0800

    feat: adjust textcase prefix

--- config/plugins/editor/text-case.nix
+++ config/plugins/editor/text-case.nix
@@ -1,22 +1,24 @@
-{ config, pkgs, ... }:
+{ pkgs, vnix, ... }:
 {
   extraPlugins = with pkgs.vimPlugins; [
-    text-case-nvim # An all in one plugin for converting text case in Neovim
+    text-case-nvim # An all in one plugin for converting text case in Neovim (https://github.com/johmsalas/text-case.nvim)
   ];
 
-  extraConfigLua = ''
-    require("textcase").setup()
-  '';
+  extraConfigLua = vnix.lua.mkRequire' "textcase" "setup" {
+    # Set `default_keymappings_enabled` to false if you don't want automatic keymappings to be registered.
+    # default_keymappings_enabled = true;
 
-  # keymaps = [
-  #   {
-  #     mode = "n";
-  #     key = "<leader>...";
-  #     action = "<cmd>...<cr>";
-  #     options = {
-  #       silent = true;
-  #       desc = "...";
-  #     };
-  #   }
-  # ];
+    # `prefix` is only considered if `default_keymappings_enabled` is true. It configures the prefix
+    # of the keymappings, e.g. `gau ` executes the `current_word` method with `to_upper_case`
+    # and `gaou` executes the `operator` method with `to_upper_case`.
+    prefix = "<localleader>t"; # default: ga
+  };
+
+  extraConfigLuaPost = vnix.lua.mkRequire' "which-key" "add" [
+    {
+      __unkeyed-1 = "<localleader>t";
+      group = "Text Case";
+      icon = "󱄽 ";
+    }
+  ];
 }
