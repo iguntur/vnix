9b4f39c4f59d0cad3ed0f7e970041244d10903bb
Guntur Poetra <poetra.guntur@gmail.com>
2025-01-26 04:56:12 +0800

    feat: plugins - mini.files

--- config/plugins/base/file-tree.nix
+++ config/plugins/base/file-tree.nix
@@ -1,6 +1,32 @@
 { lib, pkgs, ... }:
 {
   plugins = {
+    # mini.modules = {
+    #   files = {
+    #     # General options
+    #     options = {
+    #       permanent_delete = false; # Whether to delete permanently or move into module-specific trash
+    #       use_as_default_explorer = true; # Whether to use for editing directories
+    #     };
+    #
+    #     # TODO: would be great if having this actions instead of manual define mappings.
+    #     # mappings = {
+    #     #   vertical = "<C-v>";
+    #     #   horizontal = "<C-s>";
+    #     #   hidden = "g."; # toggle hidden files
+    #     # };
+    #
+    #     # Customization of explorer windows
+    #     windows = {
+    #       max_number.__raw = "math.huge"; # Maximum number of windows to show side by side
+    #       preview = true; # Whether to show preview of file/directory under cursor
+    #       width_focus = 25; # Width of focused window
+    #       width_nofocus = 15; # Width of non-focused window
+    #       width_preview = 80; # Width of preview window
+    #     };
+    #   };
+    # };
+
     oil = {
       enable = true;
       settings = {
@@ -49,6 +75,27 @@
   };
 
   keymaps = [
+    # Mini files
+    # {
+    #   mode = "n";
+    #   key = "<leader>fm";
+    #   action = ":lua MiniFiles.open(vim.fn.expand('%:h'))<cr>";
+    #   options = {
+    #     silent = true;
+    #     desc = "Mini File Manager (Relative Working File)";
+    #   };
+    # }
+    # {
+    #   mode = "n";
+    #   key = "<leader>fM";
+    #   action = ":lua MiniFiles.open()<cr>";
+    #   options = {
+    #     silent = true;
+    #     desc = "Mini File Manager (cwd)";
+    #   };
+    # }
+
+    # Oil
     {
       mode = "n";
       key = "<leader>f-";
@@ -58,6 +105,8 @@
         desc = "Open Oil at the current file";
       };
     }
+
+    # Yazi
     {
       mode = "n";
       key = "<leader>fe";
