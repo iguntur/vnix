bc01373e02453440db182001b97615e060ab072a
Guntur Poetra <poetra.guntur@gmail.com>
2025-01-27 01:28:01 +0800

    feat: gopls settings

--- config/plugins/ft/go.nix
+++ config/plugins/ft/go.nix
@@ -6,7 +6,60 @@
     # ------------------------------------------------------------------------------
     lsp.servers = {
       golangci_lint_ls.enable = true;
-      gopls.enable = true;
+      gopls = {
+        enable = true;
+        filetypes = [ "go" "gomod" "gowork" "gosum" "gotmpl" ];
+        settings.gopls = {
+          gofumpt = true;
+          completeUnimported = true;
+          directoryFilters = [ "-.git" "-.vscode" "-.idea" "-.vscode-test" "-node_modules" ];
+          semanticTokens = true;
+          staticcheck = true;
+          usePlaceholders = true;
+          analyses = {
+            # fieldalignment = true;
+            nilness = true;
+            unusedparams = true;
+            unusedwrite = true;
+            useany = true;
+          };
+          codelenses = {
+            gc_details = false;
+            generate = true;
+            regenerate_cgo = true;
+            run_govulncheck = true;
+            test = true;
+            tidy = true;
+            upgrade_dependency = true;
+            vendor = true;
+          };
+          hints = {
+            assignVariableTypes = true;
+            compositeLiteralFields = true;
+            compositeLiteralTypes = true;
+            constantValues = true;
+            functionTypeParameters = true;
+            parameterNames = true;
+            rangeVariableTypes = true;
+          };
+        };
+        onAttach.function =
+          # TODO: workaround: https://github.com/golang/go/issues/54531#issuecomment-1464982242
+          # lua
+          ''
+            if client.name == 'gopls' and not client.server_capabilities.semanticTokensProvider then
+              local semantic = client.config.capabilities.textDocument.semanticTokens
+              client.server_capabilities.semanticTokensProvider = {
+                full = true,
+                legend = {
+                  tokenModifiers = semantic.tokenModifiers,
+                  tokenTypes = semantic.tokenTypes,
+                },
+                range = true,
+              }
+            end
+          '';
+      };
     };
 
     # ------------------------------------------------------------------------------
