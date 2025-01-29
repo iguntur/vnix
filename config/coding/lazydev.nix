2fdfa5549a80d88c97a0089cf47068977815835e
Guntur Poetra <poetra.guntur@gmail.com>
2025-01-29 23:47:33 +0800

    feat: lazyLoad lazydev

--- config/coding/lazydev.nix
+++ config/coding/lazydev.nix
@@ -1,20 +1,19 @@
 { pkgs, lib, ... }:
 {
-  plugins = {
-    lazydev = {
-      enable = true;
-      settings = {
-        library = [
-          {
-            path = "\${3rd}/luv/library";
-            words = [ "vim%.uv" ];
-          }
-          {
-            path = "snacks.nvim";
-            words = [ "Snacks" ];
-          }
-        ];
-      };
+  plugins.lazydev = {
+    enable = true;
+    lazyLoad.settings.ft = "lua";
+    settings = {
+      library = [
+        {
+          path = "\${3rd}/luv/library";
+          words = [ "vim%.uv" ];
+        }
+        {
+          path = "snacks.nvim";
+          words = [ "Snacks" ];
+        }
+      ];
     };
   };
 }
