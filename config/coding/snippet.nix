28b965a38678ba9ab1da8151e3dafa41061bf9d6
Guntur Poetra <poetra.guntur@gmail.com>
2025-01-31 04:19:19 +0800

    feat: improve cmp with snippet

--- config/coding/snippet.nix
+++ config/coding/snippet.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, lib, pkgs, ... }:
 {
   plugins = {
     luasnip = {
@@ -10,5 +10,18 @@
     };
 
     friendly-snippets.enable = true;
+
+    mini.modules.snippets = {
+      snippets = {
+        __unkeyed-1.__raw = lib.mkIf config.plugins.friendly-snippets.enable # lua
+          ''
+            require("mini.snippets").gen_loader.from_file("${config.plugins.friendly-snippets.package}/snippets/global.json")
+          '';
+        __unkeyed-2.__raw = # lua
+          ''
+            require("mini.snippets").gen_loader.from_lang()
+          '';
+      };
+    };
   };
 }
--- config/editor/cmp.nix
+++ config/editor/cmp.nix
@@ -1,82 +1,121 @@
-{ config, lib, ... }:
+{ config, pkgs, lib, ... }:
 {
-  plugins = {
-    blink-cmp = {
-      enable = true;
-      settings = {
-        appearance = {
-          use_nvim_cmp_as_default = false;
-          nerd_font_variant = "mono";
-        };
+  plugins.blink-cmp = {
+    enable = true;
+    settings = {
+      appearance = {
+        use_nvim_cmp_as_default = false;
+        nerd_font_variant = "mono";
+      };
 
-        completion = {
-          accept = {
-            auto_brackets = {
-              enabled = true;
-              semantic_token_resolution = {
-                enabled = false;
-              };
-            };
-          };
-          menu = {
-            draw = {
-              treesitter = [ "lsp" ];
-            };
-          };
-          documentation = {
-            auto_show = true;
-            auto_show_delay_ms = 200;
-          };
-          ghost_text = {
+      completion = {
+        accept = {
+          auto_brackets = {
             enabled = true;
-          };
-        };
-
-        signature = {
-          enabled = true; # Experimental signature help support
-        };
-
-        sources = {
-          cmdline = [ ];
-          default = [
-            "lazydev"
-            "lsp"
-            "path"
-            "snippets"
-            "buffer"
-          ];
-          providers = {
-            lazydev = {
-              name = "LazyDev";
-              module = "lazydev.integrations.blink";
-              # make lazydev completions top priority (see `:h blink.cmp`)
-              score_offset = 100;
-            };
-            buffer = {
-              score_offset = -7;
-            };
-            lsp = {
-              fallbacks = [ ];
+            semantic_token_resolution = {
+              enabled = false;
             };
           };
         };
-
-        snippets = {
-          preset = if config.plugins.luasnip.enable then "luasnip" else "default";
+        menu = {
+          draw = {
+            columns = [
+              {
+                __unkeyed-1 = "label";
+              }
+              {
+                __unkeyed-1 = "kind_icon";
+                __unkeyed-2 = "kind";
+                gap = 1;
+              }
+              { __unkeyed-1 = "source_name"; }
+            ];
+            treesitter = [ "lsp" ];
+          };
         };
-
-        keymap = {
-          preset = "default";
-          "<Down>" = [
-            "select_next"
-            "fallback"
-          ];
-          "<Up>" = [
-            "select_prev"
-            "fallback"
-          ];
+        documentation = {
+          auto_show = true;
+          auto_show_delay_ms = 200;
         };
+        ghost_text = {
+          enabled = true;
+        };
+      };
+
+      signature = {
+        enabled = true; # Experimental signature help support
+      };
+
+      # snippets.preset = "mini_snippets";
+      snippets.preset = "luasnip";
+
+      sources = {
+        cmdline = [ ];
+        default = [
+          # built-in
+          "lsp"
+          "path"
+          "snippets"
+          "buffer"
+
+          # community
+          "lazydev"
+          "emoji"
+          "ripgrep"
+        ];
+        # ++ lib.optionals config.plugins.avante.enable [
+        #   "avante_commands"
+        #   "avante_files"
+        #   "avante_mentions"
+        # ];
+        providers = {
+          lazydev = {
+            name = "LazyDev";
+            module = "lazydev.integrations.blink";
+            # make lazydev completions top priority (see `:h blink.cmp`)
+            score_offset = 100;
+          };
+          buffer = {
+            score_offset = -7;
+          };
+          lsp = {
+            fallbacks = [ ];
+            # score_offset = 4;
+          };
+          emoji = {
+            name = "Emoji";
+            module = "blink-emoji";
+            score_offset = 1;
+          };
+          ripgrep = {
+            name = "Ripgrep";
+            module = "blink-ripgrep";
+            score_offset = 1;
+          };
+        };
+      };
+
+      keymap = {
+        preset = "default";
+        "<Down>" = [
+          "select_next"
+          "fallback"
+        ];
+        "<Up>" = [
+          "select_prev"
+          "fallback"
+        ];
       };
     };
   };
+
+  plugins.lsp.capabilities = lib.optionals config.plugins.blink-cmp.enable # lua
+    ''
+      capabilities = vim.tbl_deep_extend( "force", capabilities, require('blink.cmp').get_lsp_capabilities())
+    '';
+
+  extraPlugins = lib.mkIf config.plugins.blink-cmp.enable (with pkgs.vimPlugins; [
+    blink-emoji-nvim
+    blink-ripgrep-nvim
+  ]);
 }
