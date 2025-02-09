b8ef250d5e5f1d3d4996d5e9bd93cfbce55430ee
Guntur Poetra <poetra.guntur@gmail.com>
2025-02-10 03:54:47 +0800

    feat: attach keymaps

--- config/sdk/nodejs.nix
+++ config/sdk/nodejs.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, ... }:
+{ config, lib, pkgs, vnix, ... }:
 {
   plugins = {
     package-info = {
@@ -12,6 +12,21 @@
     conform-nvim.settings.formatters_by_ft = {
       # ...
     };
+
+    lsp.servers.jsonls.onAttach.function = lib.optionals config.plugins.package-info.enable (
+      vnix.lsp.onAttach_keymaps [
+        {
+          key = "<localleader>pt";
+          action.__raw = "require('package-info').toggle";
+          options.desc = "Toggle dependency version";
+        }
+        {
+          key = "<localleader>pv";
+          action.__raw = "require('package-info').change_version";
+          options.desc = "Bump dependency version";
+        }
+      ]
+    );
   };
 
   # extraPackages = with pkgs; [
