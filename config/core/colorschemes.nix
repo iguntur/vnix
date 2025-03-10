4a64e38fb624ff235abac7b4b8d52b9c8872a813
Guntur Poetra <poetra.guntur@gmail.com>
2025-03-11 02:48:10 +0800

    feat: use catppucin colorscheme

--- config/core/colorschemes.nix
+++ config/core/colorschemes.nix
@@ -1,6 +1,18 @@
 { pkgs, ... }:
 {
   colorschemes = {
+    catppuccin = {
+      enable = true;
+      settings = {
+        transparent_background = true;
+        dim_inactive = {
+          enabled = false; # dims the background color of inactive window
+          shade = "dark";
+          percentage = 0.9; # percentage of the shade to apply to the inactive window
+        };
+      };
+    };
+
     rose-pine = {
       # enable = true;
       settings = {
@@ -16,7 +28,7 @@
     };
 
     tokyonight = {
-      enable = true;
+      # enable = true;
       settings = {
         dim_inactive = true;
         terminal_colors = true;
