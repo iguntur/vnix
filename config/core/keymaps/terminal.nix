bac1c37e82d47bab729be0e985bed4dae8978efd
Guntur Poetra <poetra.guntur@gmail.com>
2025-03-13 22:53:21 +0800

    feat: open terminal on new tab

--- config/core/keymaps/terminal.nix
+++ config/core/keymaps/terminal.nix
@@ -8,5 +8,18 @@
       options.desc = "Normal mode";
       options.silent = true;
     }
+    {
+      mode = "n";
+      key = "<localleader><tab>\\";
+      action.__raw = ''
+        function()
+          vim.cmd.tabnew()
+          vim.cmd.terminal()
+          vim.cmd.startinsert()
+        end
+      '';
+      options.desc = "Open terminal on new tab";
+      options.silent = true;
+    }
   ];
 }
