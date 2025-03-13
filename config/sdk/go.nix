90ca51a3296d5fb5a3959becec56a819244e2a9e
Guntur Poetra <poetra.guntur@gmail.com>
2025-03-13 22:55:31 +0800

    fix: sematic token

--- config/sdk/go.nix
+++ config/sdk/go.nix
@@ -71,7 +71,7 @@ in
             if client.name == 'gopls' and not client.server_capabilities.semanticTokensProvider then
               local semantic = client.config.capabilities.textDocument.semanticTokens
               client.server_capabilities.semanticTokensProvider = {
-                
+                full = true,
                 legend = {
                   tokenModifiers = semantic.tokenModifiers,
                   tokenTypes = semantic.tokenTypes,
