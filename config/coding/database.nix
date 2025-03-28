4c927f5b048f52ff5ec4ed8be2dd178667348213
Guntur Poetra <poetra.guntur@gmail.com>
2025-03-28 08:06:34 +0800

    feat: fuzzy sql file and auto connect w/ dbui finder

--- config/coding/database.nix
+++ config/coding/database.nix
@@ -15,14 +15,29 @@ in
     vim-dadbod-completion = {
       enable = config.plugins.vim-dadbod.enable;
     };
+
+    fzf-lua.keymaps = {
+      "<localleader>df" = {
+        action = "files";
+        settings = {
+          prompt = "❯ ";
+          cwd.__raw = ''vim.fn.expand("${dataPath}")'';
+        };
+        options = {
+          silent = true;
+          desc = "Database queries files";
+        };
+      };
+    };
   };
 
   globals = {
-    db_ui_auto_execute_table_helpers = 1;
+    db_ui_auto_execute_table_helpers = true;
     db_ui_execute_on_save = false;
+    db_ui_disable_info_notifications = true;
     db_ui_show_database_icon = true;
     db_ui_use_nerd_fonts = true;
-    db_ui_use_nvim_notify = false;
+    db_ui_use_nvim_notify = true;
     db_ui_save_location.__raw = "vim.fn.expand('${dataPath}')";
     db_ui_tmp_query_location.__raw = "vim.fn.expand('${dataPath}/tmp')";
   };
@@ -48,6 +63,20 @@ in
       options = {
         silent = true;
         desc = "DBUI Hide Notifications";
+      };
+    }
+    {
+      mode = "n";
+      key = "<localleader>ds";
+      action.__raw = ''
+        function()
+          vim.cmd("DBUIFindBuffer")
+          vim.cmd("DBUIClose")
+        end
+      '';
+      options = {
+        silent = true;
+        desc = "DBUI Find/Search Buffer";
       };
     }
   ];
