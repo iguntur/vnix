b599946db9a78f7a2440fa2969e1bc09da114542
Guntur Poetra <poetra.guntur@gmail.com>
2025-03-30 07:38:52 +0800

    feat: cursor style (in)active window

--- config/core/autocmd.nix
+++ config/core/autocmd.nix
@@ -6,6 +6,7 @@
     vnix_spell.clear = true;
     vnix_highlight_yank.clear = true;
     vnix_terminal.clear = true;
+    vnix_window.clear = true;
   };
 
   autoCmd = [
@@ -67,6 +68,28 @@
           vim.opt.relativenumber = false
           vim.opt.laststatus = 3
           vim.opt.statuscolumn = ""
+        end
+      '';
+    }
+    {
+      event = [ "WinEnter" "BufEnter" ];
+      group = "vnix_window";
+      callback.__raw = ''
+        function()
+          vim.opt.cursorline = true; -- enable highlighting of the current line
+          vim.opt.cursorcolumn = true; -- enable highlighting of the current column
+          vim.api.nvim_feedkeys("zz", "n", false)
+        end
+      '';
+    }
+    {
+      event = [ "WinLeave" "BufLeave" ];
+      group = "vnix_window";
+      callback.__raw = ''
+        function()
+          vim.opt.cursorline = false;
+          vim.opt.cursorcolumn = false;
+          vim.api.nvim_feedkeys("zz", "n", false)
         end
       '';
     }
