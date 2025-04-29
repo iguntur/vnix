412deb5d0f0a355cf8495fc22e138dcf6a95246b
Guntur Poetra <poetra.guntur@gmail.com>
2025-04-30 01:05:35 +0800

    feat: symbol usage

--- config/coding/lsp.nix
+++ config/coding/lsp.nix
@@ -261,4 +261,125 @@
     #   enable = true;
     # };
   };
+
+  extraPlugins = with pkgs.vimPlugins; [
+    (pkgs.vimUtils.buildVimPlugin {
+      name = "symbol-usage";
+      src = pkgs.fetchFromGitHub {
+        owner = "Wansmer";
+        repo = "symbol-usage.nvim";
+        rev = "main";
+        hash = "sha256-VzliC8LARRXt/5yJN28K7UcM7sP7RQWW5CLw+a0mv0Q=";
+      };
+      dependencies = with pkgs.vimPlugins; [
+        # ... 
+      ];
+    })
+  ];
+
+  extraConfigLua = lib.strings.concatStringsSep "\n" [
+    # lua
+    ''
+      local function h(name)
+        return vim.api.nvim_get_hl(0, { name = name })
+      end
+
+      -- hl-groups can have any name
+      vim.api.nvim_set_hl(0, "SymbolUsageRounding", { fg = h("CursorLine").bg, italic = true })
+      vim.api.nvim_set_hl(0, "SymbolUsageContent", { bg = h("CursorLine").bg, fg = h("Comment").fg, italic = true })
+      vim.api.nvim_set_hl(0, "SymbolUsageRef", { fg = h("Function").fg, bg = h("CursorLine").bg, italic = true })
+      vim.api.nvim_set_hl(0, "SymbolUsageDef", { fg = h("Type").fg, bg = h("CursorLine").bg, italic = true })
+      vim.api.nvim_set_hl(0, "SymbolUsageImpl", { fg = h("@keyword").fg, bg = h("CursorLine").bg, italic = true })
+
+      local function text_format(symbol)
+        local res = {}
+
+        local round_start = { "", "SymbolUsageRounding" }
+        local round_end = { "", "SymbolUsageRounding" }
+
+        -- Indicator that shows if there are any other symbols in the same line
+        local stacked_functions_content = symbol.stacked_count > 0
+            and ("+%s"):format(symbol.stacked_count)
+            or ""
+
+        if symbol.references then
+          local usage = symbol.references <= 1 and "usage" or "usages"
+          local num = symbol.references == 0 and "no" or symbol.references
+          table.insert(res, round_start)
+          table.insert(res, { "󰌹 ", "SymbolUsageRef" })
+          table.insert(res, { ("%s %s"):format(num, usage), "SymbolUsageContent" })
+          table.insert(res, round_end)
+        end
+
+        if symbol.definition then
+          if #res > 0 then
+            table.insert(res, { " ", "NonText" })
+          end
+          table.insert(res, round_start)
+          table.insert(res, { "󰳽 ", "SymbolUsageDef" })
+          table.insert(res, { symbol.definition .. " defs", "SymbolUsageContent" })
+          table.insert(res, round_end)
+        end
+
+        if symbol.implementation then
+          if #res > 0 then
+            table.insert(res, { " ", "NonText" })
+          end
+          table.insert(res, round_start)
+          table.insert(res, { "󰡱 ", "SymbolUsageImpl" })
+          table.insert(res, { symbol.implementation .. " impls", "SymbolUsageContent" })
+          table.insert(res, round_end)
+        end
+
+        if stacked_functions_content ~= "" then
+          if #res > 0 then
+            table.insert(res, { " " , "NonText" })
+          end
+
+          table.insert(res, round_start)
+          table.insert(res, { " ", "SymbolUsageImpl" })
+          table.insert(res, { stacked_functions_content, "SymbolUsageContent" })
+          table.insert(res, round_end)
+        end
+
+        return res
+      end
+
+      require("symbol-usage").setup({
+        -- vt_position = "textwidth", -- "end_of_line",
+        text_format = text_format,
+      })
+    ''
+  ];
+
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>cu";
+      action.__raw = "function() require('symbol-usage').refresh() end";
+      options = {
+        silent = true;
+        desc = "Refresh Symbol Usage";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>uu";
+      action.__raw = "function() require('symbol-usage').toggle() end";
+      options = {
+        silent = true;
+        desc = "Toggle Symbol Usage";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>uU";
+      action.__raw = "function() require('symbol-usage').toggle_globally() end";
+      options = {
+        silent = true;
+        desc = "Toggle Symbol Usage (Global)";
+      };
+    }
+  ];
 }
+
