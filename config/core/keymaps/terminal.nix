6eee9645fbb2a882dce18f01bee90cf1773b3192
Guntur Poetra <poetra.guntur@gmail.com>
2025-07-27 20:08:27 +0700

    feat: adjust terminal

--- config/core/keymaps/terminal.nix
+++ config/core/keymaps/terminal.nix
@@ -21,5 +21,33 @@
       options.desc = "Open terminal on new tab";
       options.silent = true;
     }
+    {
+      mode = "n";
+      key = "<localleader><tab>v";
+      action.__raw = ''
+        function()
+          local dir = vim.fn.expand("%:p:h")
+          vim.cmd.vnew()
+          vim.cmd.edit("term://" .. dir .. "//fish")
+          vim.cmd.startinsert()
+        end
+      '';
+      options.desc = "Open terminal on current directory (vertical split)";
+      options.silent = true;
+    }
+    {
+      mode = "n";
+      key = "<localleader><tab>s";
+      action.__raw = ''
+        function()
+          local dir = vim.fn.expand("%:p:h")
+          vim.cmd.new()
+          vim.cmd.edit("term://" .. dir .. "//fish")
+          vim.cmd.startinsert()
+        end
+      '';
+      options.desc = "Open terminal on current directory (horizontal split)";
+      options.silent = true;
+    }
   ];
 }
