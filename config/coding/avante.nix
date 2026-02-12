77a33f1ba153cb9aaa1e091127be181baf657202
Guntur Poetra <poetra.guntur@gmail.com>
2026-02-13 03:48:53 +0700

    refactor(config): update avante.nix configuration for new providers and settings

--- config/coding/avante.nix
+++ config/coding/avante.nix
@@ -1,44 +1,51 @@
 { config, lib, pkgs, vnix, ... }:
-let
-  endpoint = "http://127.0.0.1:11434";
-  model = "qwen2.5-coder:7b";
-  # model = "qwen2.5-coder:1.5b";
-in
 {
   plugins.avante = {
     enable = true;
-    package = pkgs.vimPlugins.avante-nvim.overrideAttrs (prevAttrs: {
-      src = pkgs.fetchFromGitHub {
-        owner = "yetone";
-        repo = "avante.nvim";
-        rev = "main";
-        hash = "sha256-kd48FfxzDaVVhYQdW1LNjBoePa2b0BS/aqASj6eBmOQ=";
-      };
-      nvimSkipModules = prevAttrs.nvimSkipModules ++ [
-        # Requires setup with corresponding provider
-        "avante.providers.ollama"
-        "avante.providers.vertex_claude"
-      ];
-    });
+    # package = pkgs.vimPlugins.avante-nvim.overrideAttrs (prevAttrs: {
+    #   src = pkgs.fetchFromGitHub {
+    #     owner = "yetone";
+    #     repo = "avante.nvim";
+    #     rev = "main";
+    #     hash = "sha256-kd48FfxzDaVVhYQdW1LNjBoePa2b0BS/aqASj6eBmOQ=";
+    #   };
+    #   nvimSkipModules = prevAttrs.nvimSkipModules ++ [
+    #     # Requires setup with corresponding provider
+    #     "avante.providers.ollama"
+    #     "avante.providers.vertex_claude"
+    #   ];
+    # });
     settings = {
-      auto_suggestions_provider = "ollama";
-      provider = "ollama"; # "claude" | "openai" | "azure" | "gemini" | "vertex" | "cohere" | "copilot" | "bedrock" | "ollama"
-      ollama = {
-        inherit endpoint model;
-        timeout = 30000; # Timeout in milliseconds
-        disable_tools = true;
-        options = {
-          temperature = 0;
-          num_ctx = 20480;
-        };
-      };
-      rag_service = {
-        enabled = false; # Enables the RAG service
-        host_mount.__raw = ''vim.fn.expand("$HOME/dev")''; # Host mount path for the rag service
-        provider = "ollama"; # The provider to use for RAG service (e.g. openai or ollama)
-        llm_model = model; # The LLM model to use for RAG service
-        embed_model = "nomic-embed-text"; # The embedding model to use for RAG service
-        inherit endpoint;
+      # auto_suggestions_provider = "ollama";
+      # provider = "ollama"; # "claude" | "openai" | "azure" | "gemini" | "vertex" | "cohere" | "copilot" | "bedrock" | "ollama"
+      # rag_service = {
+      #   enabled = false; # Enables the RAG service
+      #   host_mount.__raw = ''vim.fn.expand("$HOME/dev")''; # Host mount path for the rag service
+      #   provider = "ollama"; # The provider to use for RAG service (e.g. openai or ollama)
+      #   llm_model = model; # The LLM model to use for RAG service
+      #   embed_model = "nomic-embed-text"; # The embedding model to use for RAG service
+      #   inherit endpoint;
+      # };
+      providers = {
+        ollama =
+          let
+            endpoint = "http://127.0.0.1:11434";
+            model = "qwen2.5-coder:14b";
+            # model = "qwen2.5-coder:1.5b";
+          in
+          {
+            # inherit endpoint;
+            inherit model;
+            # auth_type = "api";
+            # timeout = 30000; # Timeout in milliseconds
+            extra_request_body = {
+              options = {
+                temperature = 0.75;
+                num_ctx = 20480;
+                keep_alive = "30m";
+              };
+            };
+          };
       };
     };
   };
